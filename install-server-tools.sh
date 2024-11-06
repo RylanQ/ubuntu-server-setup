e6e4a243542fcfadfd93c7638724422c1eb375e3
RylanQ <118564707+RylanQ@users.noreply.github.com>
2024-11-06 23:36:35 +0000

    install-server-tools.sh

--- install-server-tools.sh
+++ install-server-tools.sh
@@ -6,7 +6,7 @@ sudo timedatectl set-timezone Asia/Kolkata
 
 # Install Docker
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/trusted.gpg.d/docker.gpg > /dev/null
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install -y docker-ce
